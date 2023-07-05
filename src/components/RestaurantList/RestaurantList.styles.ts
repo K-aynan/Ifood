4a677155407d5e48e6f24c86e349dc7f908a5b3d
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-07-04 21:37:19 -0300

    fixing the styles

--- src/components/RestaurantList/RestaurantList.styles.ts
+++ src/components/RestaurantList/RestaurantList.styles.ts
@@ -29,3 +29,24 @@ export const Restaurant = styled.div`
     color: #777;
   }
 `;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  margin-top: 40px;
+
+  @media (max-width: 480px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 0;
+  }
+
+  @media (max-width: 820px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+  }
+`;
--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/type";
-import { Restaurant, RestaurantListContainer } from "./RestaurantList.styles";
+import {
+  Container,
+  Restaurant,
+  RestaurantListContainer,
+} from "./RestaurantList.styles";
 
 const RestaurantList: React.FC = () => {
   const restaurants = useSelector((state: RootState) => state.restaurants);
@@ -12,13 +16,24 @@ const RestaurantList: React.FC = () => {
 
   return (
     <RestaurantListContainer>
-      {restaurants.map((restaurant) => (
-        <Restaurant key={restaurant.id}>
-          <img src={restaurant.image} alt={restaurant.name} />
-          <h3>{restaurant.name}</h3>
-          <p>{restaurant.description}</p>
-        </Restaurant>
-      ))}
+      <Container>
+        {restaurants.map((restaurant) => (
+          <Restaurant key={restaurant.id}>
+            <img src={restaurant.image} alt={restaurant.name} />
+            <h3>{restaurant.name}</h3>
+            <p>{restaurant.description}</p>
+          </Restaurant>
+        ))}
+      </Container>
+      <Container>
+        {restaurants.map((restaurant) => (
+          <Restaurant key={restaurant.id}>
+            <img src={restaurant.image} alt={restaurant.name} />
+            <h3>{restaurant.name}</h3>
+            <p>{restaurant.description}</p>
+          </Restaurant>
+        ))}
+      </Container>
     </RestaurantListContainer>
   );
 };
